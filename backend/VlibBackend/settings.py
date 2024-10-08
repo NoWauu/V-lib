cbcb6d9bacbcb5daf832ec850a568fc21ebdf021
msamatdev <mathis.samat@gmail.com>
2024-10-08 11:48:05 +0200

    Suppression de db.sqlite3, modification des paramètres du serveur

--- backend/VlibBackend/settings.py
+++ backend/VlibBackend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'VlibData',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +74,7 @@ WSGI_APPLICATION = 'VlibBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
