a819b55c4f3ba851ac6866375984b7e7905ec102
msamatdev <mathis.samat@gmail.com>
2024-12-18 10:05:34 +0100

    Modification des paramètres du serveur django et de la documentation pour pouvoir utiliser une base de donnéesz

--- backend/VlibBackend/settings.py
+++ backend/VlibBackend/settings.py
@@ -80,7 +80,16 @@ WSGI_APPLICATION = 'VlibBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv('DB_NAME'),
+        "USER": os.getenv('DB_USER'),
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "HOST": os.getenv('DB_HOST'),
+        "PORT": os.getenv('DB_PORT'),
+    }
+}
 
 
 # Password validation
