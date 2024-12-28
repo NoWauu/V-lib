8a21e5b2e627f37b017fdfc430f793df5e64f9e6
msamatdev <mathis.samat@gmail.com>
2024-12-28 20:30:14 +0100

    Ajout des scripts de journalisation et de mise à jour de la base de données

--- backend/VlibData/apps.py
+++ backend/VlibData/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class VlibdataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'VlibData'
+    def ready(self):
+        # Update database at startup*
+        from .functions.update_db import update_database
+        update_database()
+    
--- backend/VlibData/models.py
+++ backend/VlibData/models.py
@@ -1,3 +1,30 @@
+""" 
+Classes of the objects that will be stored in the database.
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    id_station = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    capacity = models.IntegerField(db_column='capacity', null=True)
+    id_location = models.ForeignKey('Location', models.CASCADE, db_column='id_location', null=True)
+    
+    class Meta:
+        db_table = 'stations'
+    
+    def __str__(self):
+        return (self.name + " - " + str(self.capacity)) + " bikes" + " - " + "latitude: " + str(self.latitude) + " - " + "longitude: " + str(self.longitude)
+    
+    
+class Location(models.Model):
+    id_location = models.AutoField(primary_key=True)
+    latitude = models.FloatField(db_column='latitude', null=True)
+    longitude = models.FloatField(db_column='longitude', null=True)
+    
+    class Meta:
+        db_table = 'locations'
+        
+    def __str__(self):
+        return f"latitude: {self.latitude}, longitude: {self.longitude}"
