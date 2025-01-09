28b4aa4a6321e0ae2ed9ce5224cdae27bdf36154
msamatdev <mathis.samat@gmail.com>
2025-01-09 15:13:26 +0100

    Ajout du fichier de test des fonctions de l'application en lien avec les utilisateurs

--- backend/VlibUsers/testing/test_functions.py
+++ backend/VlibUsers/testing/test_functions.py
@@ -1,3 +1,12 @@
 """
 Main file for testing the functions in the VlibUsers app.
 """
+
+from django.test import TransactionTestCase
+
+
+
+class TestFunction(TransactionTestCase):
+
+    def test_create_users(self):
+        pass
