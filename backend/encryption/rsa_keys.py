6257a2705c783bc89d84c4708cecaec1e82c11c3
msamatdev <mathis.samat@gmail.com>
2025-01-09 15:40:13 +0100

    Ajout du script de génération de clés publiques et privées RSA

---
+++ backend/encryption/rsa_keys.py
@@ -0,0 +1,16 @@
+"""
+This script generates RSA keys.
+
+The public key must be placed in the .env of the webapp
+The private key must be placed in the .env of the backend
+"""
+
+import rsa
+from rsa import PublicKey
+
+KEYSIZE = 1024
+
+(PUBLIC_KEY, PRIVATE_KEY) = rsa.newkeys(KEYSIZE)
+
+print(PUBLIC_KEY.save_pkcs1().decode())
+print(PRIVATE_KEY.save_pkcs1().decode())
