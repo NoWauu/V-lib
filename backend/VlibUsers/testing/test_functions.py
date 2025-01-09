b9c5b4e95cfa9362bffb30c3d894c4ec09f0930f
MaximeStepkowski <stepkowskimaxime@gmail.com>
2025-01-09 18:01:04 +0100

    Ajout de tests unitaires pour les fonctions encrypt_to_rsa et decrypt_rsa

--- backend/VlibUsers/testing/test_functions.py
+++ backend/VlibUsers/testing/test_functions.py
@@ -3,10 +3,18 @@ Main file for testing the functions in the VlibUsers app.
 """
 
 from django.test import TransactionTestCase
-
+from VlibUsers.functions.rsa_fn import encrypt_to_rsa, decrypt_rsa
 
 
 class TestFunction(TransactionTestCase):
+
+    def test_encrypt_decrypt_message(self):
+        message = "TestMessage"
+        encrypted = encrypt_to_rsa(message)
+        decrypted = decrypt_rsa(encrypted)
+        self.assertNotEqual(encrypted, message)
+        self.assertEqual(decrypted, message)
+
 
     def test_create_users(self):
         pass
