a2229c7cf5f3964fb447208fc08b3a22d652b455
MaximeStepkowski <stepkowskimaxime@gmail.com>
2025-02-12 15:35:56 +0100

    Petits correctifs (noms variables et parenthèses)

--- webapp/src/components/forms/login/LoginForm.tsx
+++ webapp/src/components/forms/login/LoginForm.tsx
@@ -14,9 +14,9 @@ export default function LoginForm() {
   async function handleSubmit() {
     const apiUrl = `http://${process.env.NEXT_PUBLIC_DJANGO_API_ROOT}/users/login`;
 
-    const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    const regexEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-    if (!(regex.test(loginEmail)) || loginPassword == '') {
+    if (!(regexEmail.test(loginEmail)) || loginPassword == '') {
       alert("Identifiants de connexion incorrects !");
       return;
     }
@@ -74,7 +74,7 @@ export default function LoginForm() {
 
     const responseEmail = await dataJson.data.email;
 
-    if(responseEmail.toString() !== (loginEmail)) {
+    if(responseEmail.toString() !== loginEmail) {
       console.log('email is invalid');
       throw new Error('email is invalid')
     }
