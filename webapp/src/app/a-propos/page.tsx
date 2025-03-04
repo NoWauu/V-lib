eb4fdbd4a8c99be2bed9bc36d9f27c997997a8bd
FLUMINOX7 <ryanchess107@gmail.com>
2025-03-04 18:46:06 +0100

    Modification effectuée

--- webapp/src/app/a-propos/page.tsx
+++ webapp/src/app/a-propos/page.tsx
@@ -14,7 +14,7 @@ export default function AProposPage() {
             <Title text="Qui sommes-nous ?" />
             <Paragraph text="Nous sommes cinq étudiants passionnés par le développement et l'innovation." />
             <Paragraph text="Notre équipe est composée de Mathis, Ryan, Téo, Maxime et Mathew." />
-            <Paragraph text="Nous sommes actuellement en 2e année de BUT Informatique." />
+            <Paragraph text="Actuellement en 2e année de BUT Informatique." />
             <Paragraph text="Nous avons créé ce projet universitaire pour faciliter la location de vélos en libre-service." />
           </div>
           <div className="md:w-1/2 p-4">
