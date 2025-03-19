6c7af5a5abe7a3f68863f89b49aafdda6d381daa
msamatdev <mathis.samat@gmail.com>
2025-03-19 21:43:16 +0100

    Modification du time to live du token de l'application web

--- backend/VlibUsers/urls.py
+++ backend/VlibUsers/urls.py
@@ -5,7 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('login/', login_request, name='login'),
     path('register/', register_request, name='register'),
-    path('refresh-token/', refresh_token_request, name='refresh-token'),
     path('favorites/', favorites_request, name='favorites'),
     path('verify-email/', verify_mail_request, name='verify-email'),
     path('delete-account/', delete_account_request, name='delete-account'),
