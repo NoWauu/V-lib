a53b510837511c4831d29137d80c4aeae09dcbbb
NoWay_y <teolem77@gmail.com>
2025-06-19 11:30:09 +0200

    fix: remove unnecessary package

--- phone_app/lib/pages/rents.dart
+++ phone_app/lib/pages/rents.dart
@@ -7,7 +7,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../auth/providers/auth_provider.dart';
 import '../config.dart';
 
@@ -161,7 +160,6 @@ class RentsPage extends State<MapSample> {
   bool _isFavorite(Station station) => _favoriteStationIds.contains(station.id);
 
   void _toggleFavorite(Station station) async {
-    final prefs = await SharedPreferences.getInstance();
     final token = context.watch<AuthProvider>().token?.token;
     if (token == null) {
       ScaffoldMessenger.of(context).showSnackBar(
